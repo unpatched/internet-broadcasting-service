0fb5fc0132b27e6a491868db1b6f9b51bc69b51a
unpatched <unpatched@unpatched>
2017-05-14 22:31:33 -0500

    Add Transcoder configuration methods

--- target/debug/libinternetbroadcastingservice.d
+++
@@ -1,1 +0,0 @@
-/home/unpatched/internetbroadcastingservice/target/debug/libinternetbroadcastingservice.rlib: /home/unpatched/internetbroadcastingservice/src/lib.rs /home/unpatched/internetbroadcastingservice/src/transcoder/mod.rs /home/unpatched/internetbroadcastingservice/src/transcoder/configuration.rs
